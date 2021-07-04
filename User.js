f424e582b7d8c420384b4acd89a7cf061b98cedd
Barış KAYA <bkaya0199@gmail.com>
2021-07-05 00:37:10 +0300

    finish

---
+++ User.js
@@ -0,0 +1,51 @@
+export const User = {
+    id: 1,
+    name: 'Barış',
+    surname: 'KAYA',
+}
+
+class UserBankAccounts {
+    constructor() {
+        this.accounts = [{
+                id: 1,
+                name: 'AAAA Hesabı',
+                type: 'try',
+                balance: 4500
+            },
+            {
+                id: 2,
+                name: 'BBBB Hesabı',
+                type: 'usd',
+                balance: 1500
+            },
+            {
+                id: 3,
+                name: 'CCCC Hesabı',
+                type: 'eur',
+                balance: 500
+            }
+        ];
+    }
+
+    getAccounts() {
+        return this.accounts;
+    }
+
+    getAccountById(accountId) {
+        let selectedAccount = this.accounts.find((account) => {
+            return account.id === Number(accountId)
+        });
+        return selectedAccount;
+    }
+
+    isDefinedBankAccountById(accountId) {
+        return this.accounts.some(account => account.id === Number(accountId));
+    }
+
+    isValidBalanceLimit(accountId, money) {
+        let selectedAccount = this.getAccountById(accountId);
+        return money > selectedAccount.balance;
+    }
+}
+
+export const UserBankAccountsInstance = new UserBankAccounts();
--- index.js
+++ index.js
@@ -1,6 +1,163 @@
-import { name } from './example-other-file';
+import { User, UserBankAccountsInstance as UserBankAccounts } from './User';
+import { IBAN_NUMBER_SIZE, MAX_FREE_TRANSFER_MONEY_LIMIT, MAX_TRANSFER_TIME_PERIOD } from './constants';
 
-console.log('Javascriptinizi buraya yazabilirsiniz')
+const bankAccountDropdown = document.querySelector('#bankAccount');
+const transferAccountIbanNumberTextbox = document.querySelector('#transferAccountIbanNumber');
+const transferMoneyTextbox = document.querySelector('#transferMoney');
+const sendPriceButton = document.querySelector('#sendPriceButton');
+const processTimeCounter = document.querySelector('#processTimeCounter');
+const alertBox = document.querySelector('#alertBox');
+var failPasswordCount = 0;
+var timeCounter = MAX_TRANSFER_TIME_PERIOD;
 
-// Baska dosyadan calistirmak mumkun, dosyalari parcalamaktan korkmayin!
-// name
+bankAccountDropdown.addEventListener('change', handleSelectedBankAccount);
+transferAccountIbanNumberTextbox.addEventListener('keyup', handleIbanNumber);
+transferMoneyTextbox.addEventListener('keyup', handleTransferMoney);
+sendPriceButton.addEventListener('click', handlePriceButton);
+
+var timeCounterInterval = setInterval(function() {
+    let nowTime = --timeCounter;
+    if (nowTime > 0) {
+        processTimeCounter.innerText = nowTime;
+    } else {
+
+        alert('Oturumunuz sonlanmıştır');
+        location.reload();
+    }
+}, 1000);
+
+
+function handleSelectedBankAccount() {
+    let selectedBankAccountId = bankAccountDropdown.value;
+    if (UserBankAccounts.isDefinedBankAccountById(selectedBankAccountId)) {
+        let selectedBankAccount = UserBankAccounts.getAccountById(selectedBankAccountId);
+        transferMoneyTextbox.setAttribute('max', selectedBankAccount.balance);
+        checkMoneyLimit();
+        checkMoneyTransferValidation();
+    }
+}
+
+function handleIbanNumber() {
+    checkMoneyTransferValidation();
+}
+
+const checkMoneyLimit = () => {
+    let money = transferMoneyTextbox.value;
+    let selectedBankAccountId = bankAccountDropdown.value;
+    if (UserBankAccounts.isDefinedBankAccountById(selectedBankAccountId)) {
+        if (UserBankAccounts.isValidBalanceLimit(selectedBankAccountId, money)) {
+            toggleTransferButton(false);
+            toggleAlertBox('Bakiyenizin üzerinde tutar giremezsiniz.', 'alert-danger');
+        } else {
+            toggleAlertBox();
+        }
+    }
+}
+
+function handleTransferMoney() {
+    checkMoneyLimit();
+    checkMoneyTransferValidation();
+}
+
+function handlePriceButton() {
+    let money = transferMoneyTextbox.value;
+
+    if (money <= MAX_FREE_TRANSFER_MONEY_LIMIT) {
+
+        toggleAlertBox('Başarılı', 'alert-success');
+
+    } else {
+
+        let password = prompt('Telefona gelen 4 haneli şifreyi giriniz', "Şifre");
+
+        if (Number(password) === 1234) {
+
+            toggleAlertBox('Başarılı', 'alert-success');
+
+        } else {
+
+            ++failPasswordCount;
+            if (isAccountBlocked()) {
+                toggleAlertBox('Hesabınız bloke oldu', 'alert-danger');
+                blockedAccount();
+            } else {
+                toggleAlertBox('Şifre yanlış', 'alert-danger');
+            }
+
+        }
+    }
+}
+
+
+const checkMoneyTransferValidation = () => {
+    if (isSelectedBankAccount() && isFilledIbanNumber() && isFilledPrice()) {
+        toggleTransferButton(true);
+    } else {
+        toggleTransferButton(false);
+    }
+}
+
+const isSelectedBankAccount = () => {
+    return UserBankAccounts.isDefinedBankAccountById(bankAccountDropdown.value);
+}
+
+const isFilledIbanNumber = () => {
+    return transferAccountIbanNumberTextbox.value.length == IBAN_NUMBER_SIZE;
+}
+
+const isFilledPrice = () => {
+    return Number(transferMoneyTextbox.value) > 0
+}
+
+
+const isAccountBlocked = () => {
+    return failPasswordCount > 3;
+}
+
+const toggleTransferButton = (open = false) => {
+    if (!!open) {
+        sendPriceButton.classList.remove('disabled');
+    } else {
+        sendPriceButton.classList.add('disabled');
+    }
+}
+
+const toggleAlertBox = (message = '', alertClass = '') => {
+    alertBox.classList.forEach((classItem) => {
+        if (classItem !== 'alert') {
+            alertBox.classList.remove(classItem);
+        }
+    })
+    if (message) {
+
+        alertBox.classList.remove('d-none');
+        alertBox.classList.add(alertClass);
+        alertBox.innerText = message;
+
+    } else {
+        alertBox.classList.add('d-none');
+        alertBox.innerText = message;
+    }
+}
+
+const blockedAccount = () => {
+    clearInterval(timeCounterInterval);
+}
+const resetTransferForm = () => {
+    failPasswordCount = 0;
+    toggleTransferButton(false);
+    processTimeCounter.innerText = timeCounter;
+
+    transferAccountIbanNumberTextbox.value = 'TR';
+    transferAccountIbanNumberTextbox.setAttribute('maxlength', IBAN_NUMBER_SIZE);
+
+    toggleAlertBox()
+
+    UserBankAccounts.getAccounts().forEach((account, index) => {
+        var opt = document.createElement('option');
+        opt.innerText = `${account.name} - ${account.balance} (${account.type})`;
+        opt.value = account.id;
+        bankAccountDropdown.appendChild(opt);
+    });
+}
+resetTransferForm();
