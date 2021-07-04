ff4a571d5d8aa0e8db9d4b3b4ad25be4347cc522
Barış KAYA <bkaya0199@gmail.com>
2021-07-05 00:34:41 +0300

    added constant values

---
+++ constants.js
@@ -0,0 +1,5 @@
+export const IBAN_NUMBER_SIZE = 24;
+export const MAX_FREE_TRANSFER_MONEY_LIMIT = 500; // şifre istemeden gönderilebilecek maksimum tutar
+export const TIME_PERIOD = 1; // SN
+export const TIME_PER_MINUTE = TIME_PERIOD * 60;
+export const MAX_TRANSFER_TIME_PERIOD = TIME_PER_MINUTE * 2;
