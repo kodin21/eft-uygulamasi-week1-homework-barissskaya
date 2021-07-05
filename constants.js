b37e5e9553a6b7bb6b763316e0e2636443c29859
Barış KAYA <bkaya0199@gmail.com>
2021-07-05 11:11:02 +0300

    added fail password limit cons

--- constants.js
+++ constants.js
@@ -3,3 +3,4 @@ export const MAX_FREE_TRANSFER_MONEY_LIMIT = 500; // şifre istemeden gönderile
 export const TIME_PERIOD = 1; // SN
 export const TIME_PER_MINUTE = TIME_PERIOD * 60;
 export const MAX_TRANSFER_TIME_PERIOD = TIME_PER_MINUTE * 2;
+export const FAIL_PASSWORD_LIMIT = 3;
